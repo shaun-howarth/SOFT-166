3c70704768e741f64d85ce310cbec3810689cf35
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2019-12-06 17:37:24 +0000

    Adding first javascript functionality to game
    Usign Jquery, I have added the frist functionality to the noughts and crosses game. Made a global variable to make an X and 0 appear when clicking on table cell for game.

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+	var playerturn = "x";
+
+
+
+	$(".game-wrapper td").on("click" , function(){
+
+
+
+		if ($(this).text() == "") {
+			$(this).text(playerturn);
+			if (playerturn =="x"){
+				playerturn = "o";
+
+			}	
+
+
+
+			else {
+
+				playerturn = "x";
+
+			}
+		}
+
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
