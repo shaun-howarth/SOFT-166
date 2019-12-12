89ff2d2f4e2d4f37b2aa786e3d1f6b9483d76f25
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2019-12-12 14:09:43 +0000

    Have added interactivty to the player buttons
    Have added interactivty to the player buttons. Whenever it is Player 1's or Player 2's the pane button will go to the colour green.

--- js/main.js
+++ js/main.js
@@ -1,56 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 	var playerturn = "x";
 
+	$(".game-wrapper td").on("click", function() {
+		if($(this).text() == "") {
 
-
-	$(".game-wrapper td").on("click" , function(){
-
-
-
-		if ($(this).text() == "") {
 			$(this).text(playerturn);
-			if (playerturn =="x"){
-				playerturn = "o";
 
+			if(playerturn =="x") {
+				$("#player1").css("background", "rgba(156, 9, 9, 0.72)");
+				$("#player2").css("background", "rgba(12, 130, 65, 0.72)");
+				playerturn = "o";
 			}	
 
 
 
-			else {
+			else 
+		{
+			playerturn ="o" 
+			$("#player2").css("background", "rgba(156, 9, 9, 0.72)");
+			$("#player1").css("background", "rgba(12, 130, 65, 0.72)");
+			playerturn = "x";
 
-				playerturn = "x";
-
-			}
 		}
 
 
 
 
-
-
-
-		
-
+		}
 
 	});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
